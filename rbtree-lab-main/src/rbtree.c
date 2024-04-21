9202117557f9406bb32da5dae75be9f70d58b337
MJ <91minjung@gmail.com>
2024-04-21 14:56:04 +0900

    04/20 rbtree start

--- rbtree-lab-main/src/rbtree.c
+++ rbtree-lab-main/src/rbtree.c
@@ -5,16 +5,45 @@
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
+  node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+  nil->color = 1;
+  nil->key = -2147483648;
+  nil->left = NULL;
+  nil->right = NULL;
+  nil->parent = NULL;
+  p->nil = nil;
+  p->root = nil;
   return p;
+}
+
+node_t *recursive_cut(node_t *node) {
+  if (node->left->parent == NULL && node->right->parent == NULL) return node;
+  if (node->left->parent != NULL) free(recursive_cut(node->left));
+  if (node->right->parent != NULL) free(recursive_cut(node->right));
+  return node;
 }
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
+  free(recursive_cut(t->root));
+  free(t->nil);
   free(t);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
+  node_t *new_node = (node_t *) calloc(1, sizeof(node_t));
+  new_node->color = 0;
+  new_node->key = key;
+  new_node->left = t->nil;
+  new_node->right = t->nil;
+
+  if (t->root == t->nil) {
+    new_node->color = 1;
+    new_node->parent = t->nil;
+    t->root = new_node;
+    return t->root;
+  }
   return t->root;
 }
 
